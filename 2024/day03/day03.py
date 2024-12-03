69e6cd9addaba6797b187b17a126046fefb56e06
Alexander Ivanets <alex.ivanets@proton.me>
2024-12-03 12:43:58 +0100

    Add solution Day 3 Part 2

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -16,6 +16,32 @@ def part01():
     mults = [int(x) * int(y) for x, y in grps]
     print(sum(mults))
 
+def part02():
+    with open("2024/day03/input.txt", "r") as f:
+        input = f.read()   
+
+    pattern = re.compile(r"do\(\)|don't\(\)|mul\(\d+,\d+\)")
+
+    allmatches = re.findall(pattern, input)
+
+    flag = True
+    sum = 0
+
+    pattern_mul = re.compile(r'\D+(\d+),(\d+)\)')
+
+    for match in allmatches:
+        if match == "don't()":
+            flag = False
+        elif match == "do()":
+            flag = True
+        else:
+            if flag:
+                x, y = re.search(pattern_mul, match).groups()
+                sum += int(x) * int(y)
+
+    print(sum)
+
 
 if __name__ == "__main__":
-    part01()
+    # part01()
+    part02()
